34bb6a1bdd10d620bfa2f96b2b74db4668ff48ed
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-03-16 22:37:23 +0100

    q_66645812_5615357_beannpe

---
+++ src/main/java/com/zerofiltre/stackoverflow/q_66645812_5615357_beannpe/MyLocalContainerEntityManagerFactoryBean.java
@@ -0,0 +1,7 @@
+package com.zerofiltre.stackoverflow.q_66645812_5615357_beannpe;
+
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+public class MyLocalContainerEntityManagerFactoryBean extends LocalContainerEntityManagerFactoryBean {
+
+}
